80faa7e2602f1179aa932233c407443131c49271
karrina867 <92594854+karrina867@users.noreply.github.com>
2021-10-15 19:19:42 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+//Карина Баслык
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*
+	*Boolean1 Дано целое число A. Проверить истинность высказывания: «Число A является положительным».
+
+	*/
+	/*int a;
+	bool res;
+	cin >> a;
+	res = a > 0;
+	cout << boolalpha << res << endl;*/
+
+	/*
+	* Boolean2. Дано целое число A. Проверить истинность высказывания: «Число A является нечетным».
+	*/
+	/*int a;
+	bool res;
+	cin >> a;
+	res = a % 2 != 0;
+	cout << boolalpha << res << endl;
+	return 0;*/
+	/*
+	* Boolean3 Дано целое число A. Проверить истинность высказывания: «Число A является четным».
+	*/
+	int a;
+	bool res;
+	cin >> a;
+	res = a % 2 == 0;
+	cout << boolalpha << res << endl;
+	return 0;
+}
